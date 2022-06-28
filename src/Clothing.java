5368f8a999a00cdb40f56cbf57fa69db60cb24b6
Mohmmad Hashmi <abbashashmi.osu@gmail.com>
2022-06-28 11:04:00 -0400

    Adding class practice HW

---
+++ src/Clothing.java
@@ -0,0 +1,34 @@
+//Homework example for creating classes
+public class Clothing {
+
+    public double price;
+    public String type;
+    public String size;
+
+    public Clothing(){
+        this.price = 29.9;
+        this.type = "T-shirt";
+        this.size = "Medium";
+    }
+
+    public Clothing(double price, String type, String size){
+        this.price = price;
+        this.type = type;
+        this.size = size;
+    }
+
+    public void increasePrice(){
+        this.price += 5;
+    }
+
+    public void increasePrice(double x){
+        this.price += x;
+    }
+
+    public void details(){
+        System.out.println("Item: " + this.type);
+        System.out.println("Size: " + this.size);
+        System.out.println("Price: $" + this.price);
+        System.out.println();
+    }
+}
